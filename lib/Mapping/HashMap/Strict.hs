de461269fd80225720fc9d46b5b3adaa936925fa
tiagodepalves <tiagodepalves@gmail.com>
2019-09-28 14:42:03 -0300

    `Mapping` para `Data.HashMap.Strict` e `Data.HashMap.Lazy`

---
+++ lib/Mapping/HashMap/Strict.hs
@@ -0,0 +1,42 @@
+{-# LANGUAGE NoImplicitPrelude     #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FlexibleInstances     #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Mapping.HashMap.Strict
+-- Copyright   :  (c) 2019 Tiago de Paula & João Pedro de Amorim
+-- License     :  MIT
+--
+-- Implementação de 'Mapping' para 'Data.HashMap.Strict.HashMap'
+--
+-----------------------------------------------------------------------------
+
+module Mapping.HashMap.Strict (HashMap) where
+
+import Mapping
+import Data.Ord
+import Data.Hashable
+import Data.HashMap.Strict as M
+
+
+instance (Hashable k, Ord k) => Mapping HashMap k v where
+
+    empty    = M.empty
+    fromList = M.fromList
+
+    fold   = M.foldrWithKey
+    toList = M.toList
+
+    lookup = M.lookup
+    keys   = M.keys
+    elems  = M.elems
+    member = M.member
+    size   = M.size
+    null   = M.null
+
+    alter  = M.alter
+    insert = M.insert
+    adjust = M.adjust
+    update = M.update
+    delete = M.delete
